102a818aa484c3238a906a53a8467784fcf17f98
Julius Vincent D. Llavan <202280209@psu.palawan.edu.ph>
2025-04-14 18:28:39 +0800

    Add MultilineIncidentTop3Country view and update chart.html to fetch and display multiple line chart dynamically [No Display, Fix later!]

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountByMonth
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountByMonth, MultilineIncidentTop3Country
 from fire import views
 
 urlpatterns = [
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
     path('chart/', PieCountbySeverity, name='chart'),
+    path('lineChart/', LineCountByMonth, name='chart'),
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
 ]
